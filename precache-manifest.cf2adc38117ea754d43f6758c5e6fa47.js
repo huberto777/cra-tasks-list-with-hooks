f54fbc9aa427bcefa986e1e9c74a00a188ad47bd
Hubert <huberto777@op.pl>
2020-05-30 15:12:43 +0200

    Updates

---
+++ precache-manifest.cf2adc38117ea754d43f6758c5e6fa47.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1d3163bbb3cf042ba65df51a82ca77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afb6ea678597ea4d0593",
+    "url": "/static/css/3.1a880fc0.chunk.css"
+  },
+  {
+    "revision": "ae9c1aec0069942b4b9c",
+    "url": "/static/css/4.e66499ba.chunk.css"
+  },
+  {
+    "revision": "990bf01e777c08df92dc",
+    "url": "/static/css/main.5047b31c.chunk.css"
+  },
+  {
+    "revision": "a48732b88d09f50bbc32",
+    "url": "/static/js/2.b5408c33.chunk.js"
+  },
+  {
+    "revision": "afb6ea678597ea4d0593",
+    "url": "/static/js/3.71971844.chunk.js"
+  },
+  {
+    "revision": "ae9c1aec0069942b4b9c",
+    "url": "/static/js/4.2c3cfe0e.chunk.js"
+  },
+  {
+    "revision": "990bf01e777c08df92dc",
+    "url": "/static/js/main.43cd318f.chunk.js"
+  },
+  {
+    "revision": "de8200fd69037017b79b",
+    "url": "/static/js/runtime-main.987206f8.js"
+  }
+]);
